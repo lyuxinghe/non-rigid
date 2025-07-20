cd38605f81ae65ea2c0e297e90a5c27c394fc685
Lyuxing He <lyuxingh@andrew.cmu.edu>
2025-07-19 23:39:14 -0400

    minor bug fix

--- src/non_rigid/models/dit/diffusion/gaussian_diffusion_ddrd_seperate.py
+++ src/non_rigid/models/dit/diffusion/gaussian_diffusion_ddrd_seperate.py
@@ -363,9 +363,7 @@ class GaussianDiffusionDDRDSeparate:
             "pred_xstart": pred_xstart_s,
             "extra": extra,
         }
-        #out_s["mean"] = out_s["mean"] - out_s["mean"].mean(dim=2, keepdim=True)  # Center the shape mean
 
-        out_s["mean"] = out_s["mean"] - out_s["mean"].mean(dim=2, keepdim=True)  # Center the shape mean
         return out_r, out_s
 
 
